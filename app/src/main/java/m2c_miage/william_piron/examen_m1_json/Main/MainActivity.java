78af2618df79e0be8132bbd3e642b1a2da829806
William <william.piron77@gmail.com>
2018-01-19 17:01:34 +0100

    Ajout de la geolocation.
    Met à jour sur l'écran de l'app la longitude et latitude toutes les deux secondes.

--- app/src/main/java/m2c_miage/william_piron/examen_m1_json/Main/MainActivity.java
+++ app/src/main/java/m2c_miage/william_piron/examen_m1_json/Main/MainActivity.java
@@ -1,10 +1,17 @@
 package m2c_miage.william_piron.examen_m1_json.Main;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import m2c_miage.william_piron.examen_m1_json.R;
 import m2c_miage.william_piron.examen_m1_json.Singleton.GeolocSingleton;
@@ -12,6 +19,11 @@ import m2c_miage.william_piron.examen_m1_json.Singleton.GeolocSingleton;
 public class MainActivity extends AppCompatActivity {
 
     private GeolocSingleton geoloc = GeolocSingleton.getInstance();
+
+    protected Location location;
+    protected double longitude_location;
+    protected double latitude_location;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,50 @@ public class MainActivity extends AppCompatActivity {
         final TextView street = findViewById(R.id.text_street);
         final TextView price = findViewById(R.id.text_price);
 
-        start.setOnClickListener(new View.OnClickListener() {
+
+        LocationManager lm;
+        if (getApplicationContext().checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission obtenue !", Toast.LENGTH_SHORT).show();
+            lm = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    longitude_location = location.getLongitude();
+                    latitude_location = location.getLatitude();
+
+                    geoloc.setLongitude(String.valueOf(longitude_location));
+                    geoloc.setLatitude(String.valueOf(latitude_location));
+
+                    longitude.setText(geoloc.getLongitude());
+                    latitude.setText(geoloc.getLatitude());
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                    //TODO
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                    //TODO
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    //TODO
+                }
+            };
+            Toast.makeText(this, "Listener validé !", Toast.LENGTH_SHORT).show();
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+        }
+        else
+            Toast.makeText(this, "Permission refusée", Toast.LENGTH_SHORT).show();
+
+        Geocoder geocoder = new Geocoder(this);
+
+        getinfos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TEMPORAIRE
@@ -37,5 +92,14 @@ public class MainActivity extends AppCompatActivity {
                 price.setText(geoloc.getPrice());
             }
         });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
 }
